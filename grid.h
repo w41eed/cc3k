7b69180b24241c3edecc6ffee4ffa41b6142dc10
Shawn Gallea <shawngallea@gmail.com>
2016-11-19 04:38:58 +0000

    Added grid.h

---
+++ grid.h
@@ -0,0 +1,13 @@
+#include <vector>
+#include <fstream>
+
+class Grid {
+ std::vector<std::vector<char>> theGrid;
+ int gridLen;
+
+ public:
+ void init(std::ifstream &file);
+ void printIt();
+
+
+};
