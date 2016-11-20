8af7c6cfa881f34eb315089b23fce2311fef3dee
Shawn Gallea <shawngallea@gmail.com>
2016-11-20 01:00:29 +0000

    Fixed some shit

--- Grid.h
+++ Grid.h
@@ -1,12 +1,15 @@
 #include <vector>
 #include <fstream>
 #include "Cell.h"
+#include "TextDisplay.h"
 
 class Grid {
  std::vector<std::vector<Cell *>> theGrid;
  int gridLen;
+ TextDisplay *td;
 
  public:
+ Grid();
  void init(std::ifstream &file);
  void printIt();
  ~Grid();
