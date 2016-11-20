dd8e4ebaf96cdc6061cd5c257a77571729ba17a0
Shawn Gallea <shawngallea@gmail.com>
2016-11-20 04:47:29 +0000

    Branching off

--- Enemy.cc
+++
@@ -1,6 +0,0 @@
-#include "Character.h"
-#include "Enemy.h"
-
-Enemy::Enemy(int HP, int Atk, int Def) : Character(HP, Atk, Def) {
-
-}
--- Human.cc
+++
@@ -1,5 +0,0 @@
-//
-// Created by john on 19/11/16.
-//
-
-#include "Human.h"
