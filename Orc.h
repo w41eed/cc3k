0bdf0e00466a3d00faa007fdfa6b13c70d1fa3c0
[John Fernandes] <john.fernandes@tiko3d.com>
2016-11-20 01:22:56 -0500

    Implement orc

---
+++ Orc.h
@@ -0,0 +1,18 @@
+//
+// Created by john on 20/11/16.
+//
+
+#ifndef CC_3K_ORC_H
+#define CC_3K_ORC_H
+
+#include "Enemy.h"
+
+class Orc: public Enemy {
+public:
+    Orc();
+    void move() override ;
+
+};
+
+
+#endif //CC_3K_ORC_H
