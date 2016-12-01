2d47ddf9ec4d17d054e0fe9a2254c0f72c5e96d9
[John Fernandes] <john.fernandes@tiko3d.com>
2016-12-01 06:39:36 -0500

    adds preliminary inventory for potential decorator pattern
    just comments

--- Player.h
+++ Player.h
@@ -4,6 +4,8 @@
 
 
 #include "Character.h"
+#include "Item.h"
+
 class Enemy;
 
 class Player: public Character {
@@ -12,6 +14,8 @@ class Player: public Character {
 public:
  Player(int, int, int, int); //ctor
  char getChar(); // gets char to be printed
+// Item inventory;
+// TODO: put inventory of type item in player to make decorator pattern work? but it cant work since Item is pure virtual
 };
 
 #endif
