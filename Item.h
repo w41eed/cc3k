68aff761cb67769c03a9b5d8f760954abc18c38c
[John Fernandes] <john.fernandes@tiko3d.com>
2016-12-02 09:06:29 -0500

    Gives usePotion in item a definition
    also put dependencies in include guard

--- Item.h
+++ Item.h
@@ -1,14 +1,15 @@
-#include <iostream>
-#include "Cell.h"
-
 #ifndef ITEM_H
 #define ITEM_H
+
+#include <iostream>
+#include "Cell.h"
+#include "Character.h"
 
 class Item {
 	public:
 	virtual char getSym() = 0;
-	virtual ~Item() {}	
-    virtual void usePotion(Character *c);
+//	virtual ~Item() {};
+    virtual void usePotion(Character *c) {};
 
 };
 
