fc33ca3c1d67585f200a079062c044ab5766617b
[John Fernandes] <john.fernandes@tiko3d.com>
2016-12-04 00:19:41 -0500

    updates player when gold is aqcuired from enemy
    not sure if works

--- Controller.cc
+++ Controller.cc
@@ -530,6 +530,7 @@ while(floorNum <= 5) {
        // other enemies randomly drop a small pile or a normal pile
        c->setG(goldDropped);
      }
+     ab->updatePlayer(c);
      delete enemyVec[i];
      enemyVec[i] = nullptr;
     }
